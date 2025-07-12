9cfe8bcbf8c001dcc03ffd2c0131d5dc5ae0ed0c
JesOb <JesObertyukh@gmail.com>
2025-07-12 23:27:50 +0200

    Changed cleaned up coregame loader

--- Assets/!Game/!Code/CoreGame/CoregameLoader.cs
+++ Assets/!Game/!Code/CoreGame/CoregameLoader.cs
@@ -5,142 +5,63 @@ namespace Flexy.Template.BarleyBreak.CoreGame
 	public class CoregameLoader : State, IStateWithResult<Single>
 	{
 		[Bindable] Int32	LoadingProgress		=> (Int32)(LoadingProgress01 * 100);
-        [Bindable] Single	LoadingProgress01	{get;set;} 
+        [Bindable] Single	LoadingProgress01	=> _loadTask.Progress; 
 
         private Facade_Coregame	_game; 
         public	Facade_Coregame	Game		=> _game.GetCached( this );
         
-        private SceneRef    _map;
-        
-		private Single		_runResult;
+		private LoadSceneTask	_loadTask;
+		private Single			_runResult;
 
-        protected override	void	OnShow				( )		
+		protected override	void	OnShow				( )		
 		{
-			LoadField( ).Forget( Debug.LogException );
+			LoadField( ).Forget( );
+		}
+		protected override	void	OnFwdHide			( )		
+		{
+			_loadTask = default;
 		}
 		protected override	void	OnBackShow			( )		
 		{
 			_runResult = Game.Mode.RunTime;
 			
-			UnloadField( ).Forget( Debug.LogException );
+			UnloadField( ).Forget( );
 		}
-
+		protected override	void	OnHide				( )		
+		{
+			_loadTask = default;
+		}
+		
 		private async	UniTask		LoadField			( )		
 		{
-			Debug.Log( $"[DTLoader_Coregame] ----------- ===========   Coregame Loading: START   =========== -----------" );
-			
 			var loadedScene = default(Scene);
-			Time.timeScale	= 0.001f;
 			
-			if ( OpenParams == null ) //We started from coregame scene so just open hud
+			if ( OpenParams == null )
 			{
-				Debug.Log( $"[DTLoader_Coregame] ----------- ===========   LoadLocation:  current {SceneManager.GetActiveScene().name}   =========== -----------" );
-				loadedScene			= SceneManager.GetActiveScene( );
-				
-				await UniTask.Delay( 100, DelayType.UnscaledDeltaTime );
-				
-				LoadingProgress01 = 1;
-				RebindProperty( "LoadingProgress01" );
-				
-				await UniTask.Delay( 300, DelayType.UnscaledDeltaTime );
-				
-				// scene must be loaded beforehand so do nothing
+				//We started from coregame scene so just simulate short loading and open root state
+				loadedScene		= SceneManager.GetActiveScene( );
 			}
 			else
 			{
-				var oldFramerate = Application.targetFrameRate;
-				Application.targetFrameRate = 10;				//Do not need high framerate for loading screen
-				
 				var sceneRef	= (SceneRef)OpenParams;
-				
-				Debug.Log( $"[DTLoader_Coregame] ----------- ===========   LoadLocation: {AssetRef.AssetsLoader.GetSceneName( sceneRef )}   =========== -----------" );
-				
-				var task		= sceneRef.LoadSceneAsync( gameObject, LoadSceneMode.Single );
-				
-				await task.WaitForSceneLoadStart( );
-				
-				loadedScene = task.Scene;
-				
-				while( !task.IsDone )
-				{
-					LoadingProgress01 = task.Progress;
-					RebindProperty( "LoadingProgress01" );
-					await UniTask.NextFrame( PlayerLoopTiming.EarlyUpdate );
-					
-					//Make sure subscene is loaded, todo make normal explicit task for loading match and all content
-					//var matchScene = await _loadTask.Task;
-				}
-				RebindProperty( "LoadingProgress01" );
-				
-				Application.targetFrameRate = oldFramerate;
+				_loadTask		= sceneRef.LoadSceneAsync( gameObject, LoadSceneMode.Single );
+				loadedScene		= await _loadTask;
 			}
 			
-			Debug.Log( $"[DTLoader_Coregame] ----------- ===========   LoadLocation: DONE   =========== -----------" );
-			
-			SceneManager.MoveGameObjectToScene( GameStage.gameObject, loadedScene );
-			GameStage.transform.SetSiblingIndex(0);
-			
-			await UniTask.NextFrame( );
-			
-			var      timeStartLoad = Time.realtimeSinceStartup;
-			
-			//Make sure loading screen is visible at least 1 second
-			var elapsedTimeFromStartLoad	= Time.realtimeSinceStartup - timeStartLoad; 
-			if( elapsedTimeFromStartLoad < 0.5f )
-				await UniTask.Delay( TimeSpan.FromSeconds(1-elapsedTimeFromStartLoad), DelayType.Realtime );
-			
-			await UniTask.DelayFrame( 1 );
-			Time.timeScale	= 1f;
-			
-			var loadedMinigame = FindAnyObjectByType<GameMode>( );
-			GameStage.Context.SetService( loadedMinigame );
-			
+			await UniTask.DelayFrame( 5 );
+			GameStage.MoveToLoadedScene( loadedScene );
 			GameStage.OpenRootState( );
-			
-			Debug.Log( $"[DTLoader_Coregame] ----------- ===========   Coregame Loading: DONE   =========== -----------" );
 		}
 		private async	UniTask		UnloadField			( )		
 		{
-			Debug.Log( $"[DTLoader_Coregame] ----------- ===========   Coregame Unloading: START   =========== -----------" );
+			GameStage.MoveToServiceScene( );
 			
-			await UniTask.NextFrame( );
-
-			GameStage.transform.parent = null;
-			SceneManager.MoveGameObjectToScene( GameStage.gameObject, GameStage.FlowService.gameObject.scene );
-			
-			var timeStartLoad = Time.realtimeSinceStartup;
-			var task = SceneRef.LoadDummySceneAsync( gameObject, LoadSceneMode.Single );
-			
-			while( !task.IsDone )
-			{
-				LoadingProgress01 = task.Progress;
-				RebindProperty( "LoadingProgress01" );
-				await UniTask.NextFrame( PlayerLoopTiming.EarlyUpdate );
-			}
-			
-			RebindProperty( "LoadingProgress01" );
-			
-			Time.timeScale = 0.001f;
-			await UniTask.DelayFrame( 2 );
-			
-			GC.Collect( GC.MaxGeneration, GCCollectionMode.Forced, true, true );
-			Resources.UnloadUnusedAssets( );
-			GC.Collect( GC.MaxGeneration, GCCollectionMode.Forced, true, true );
-			
-			var elapsedTimeFromStartLoad = Time.realtimeSinceStartup - timeStartLoad; 
-			
-			if( elapsedTimeFromStartLoad < 1 )
-				await UniTask.Delay( TimeSpan.FromSeconds(1-elapsedTimeFromStartLoad), DelayType.Realtime );
-			
-			GameStage.GetComponent<GameContext>( ).Destroy( );
-			await UniTask.NextFrame( );
-			
-			Time.timeScale = 1;
+			_loadTask = SceneRef.LoadDummySceneAsync( gameObject, LoadSceneMode.Single );
+			await _loadTask;
+			await UniTask.DelayFrame( 5 );
 			
 			Close( );
 			GameStage.RemoveFromHistoryAfterClose( );
-			
-			Debug.Log( $"[DTLoader_Coregame] ----------- ===========   Coregame Unloading: DONE   =========== -----------" );
 		}
 		
 		public			Single		GetResult			( )	=> _runResult;
