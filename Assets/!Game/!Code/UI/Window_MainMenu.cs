a65b64f492416d212459fcf8976724bb0a893b31
JesOb <JesObertyukh@gmail.com>
2025-07-16 08:11:06 +0200

    Added Lit Motion for compatibility test

--- Assets/!Game/!Code/UI/Window_MainMenu.cs
+++ Assets/!Game/!Code/UI/Window_MainMenu.cs
@@ -1,3 +1,5 @@
+using Flexy.Core.Tweens;
+
 namespace Flexy.Template.BarleyBreak.UI
 {
 	public class Window_MainMenu : UIWindowEx
@@ -22,6 +24,13 @@ namespace Flexy.Template.BarleyBreak.UI
 				UnityEditor.EditorApplication.ExitPlaymode( );
 	#endif
 			}
+		}
+		
+		[SerializeField]	Transform _tr;
+
+		private void Start()
+		{
+			Tween.Value(-675, -345, 1, Ease.InOutSine).BindTo( _tr, static (v, tr) => tr.localPosition = new Vector3(-400, v, 0)).Run();
 		}
 	}
 }
