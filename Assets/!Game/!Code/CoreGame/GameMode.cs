fe0703f2a6bf814f7f60168c9672b6c2621a4f46
JesOb <JesObertyukh@gmail.com>
2025-07-21 08:08:53 +0200

    Changed Cleanup

--- Assets/!Game/!Code/CoreGame/GameMode.cs
+++ Assets/!Game/!Code/CoreGame/GameMode.cs
@@ -2,75 +2,75 @@ using Flexy.Core.Tweens;
 
 namespace Flexy.Template.BarleyBreak.CoreGame
 {
-    public class GameMode : MonoBehEx
-    {
-	    [SerializeField]	EField			_board;
-        [SerializeField]	Cell			_cellPrefab;
-        [SerializeField]	GameObject		_inputBlocker;
-        [SerializeField]	GridLayoutGroup	_cellContainer;
-        [SerializeField]	Int32			_gridSize = 3;
-        [SerializeField]	Single			_animateTime = 0.2f;
-        
-        [SerializeField]	FlexyEvent		_win;
+	public class GameMode : MonoBehEx
+	{
+		[SerializeField]	EField			_board;
+		[SerializeField]	Cell			_cellPrefab;
+		[SerializeField]	GameObject		_inputBlocker;
+		[SerializeField]	GridLayoutGroup	_cellContainer;
+		[SerializeField]	Int32			_gridSize		= 3;
+		[SerializeField]	Single			_animateTime	= 0.2f;
+
+		[SerializeField]	FlexyEvent		_win;
 	
-	    private List<Cell> _cells = new();
+		private List<Cell> _cells = new();
 
-	    public		EField	Board		=> _board;
-	    public		Single	StartTime	{ get; set; }
-        public		Single	RunTime		=> Time.realtimeSinceStartup - StartTime;
-        public		Boolean	IsWin		{ get; set; }
+		public		EField	Board		=> _board;
+		public		Single	StartTime	{ get; set; }
+		public		Single	RunTime		=> Time.realtimeSinceStartup - StartTime;
+		public		Boolean	IsWin		{ get; set; }
 
-        private     void    Awake       ( )     
-        {
-	        _cellPrefab.gameObject.SetActive(false);
-	        _cellContainer.constraintCount = _gridSize;
+		private     void    Awake       ( )     
+		{
+			_cellPrefab.gameObject.SetActive(false);
+			_cellContainer.constraintCount = _gridSize;
         
-            for (var i = 0; i < _gridSize*_gridSize; i++)
-            {
-                var cell = Instantiate( _cellPrefab, _cellContainer.transform );
+			for (var i = 0; i < _gridSize*_gridSize; i++)
+			{
+				var cell = Instantiate( _cellPrefab, _cellContainer.transform );
                 
-                cell.Game = this;
-                cell.Index = i;
+				cell.Game = this;
+				cell.Index = i;
                 
-                _cells.Add( cell );
-                cell.gameObject.SetActive(true);
-            }
+				_cells.Add( cell );
+				cell.gameObject.SetActive(true);
+			}
             
-            _cells[^1].MakeLastCellClear();
-        }
-        private		void	OnEnable	( )		
-        {
-            InitMinigameAsync( ).Forget( );
-        }
+			_cells[^1].MakeLastCellClear();
+		}
+		private		void	OnEnable	( )		
+		{
+			InitMinigameAsync( ).Forget( );
+		}
         
-        #if UNITY_EDITOR
-        private		void	Update		( )		
-        {
-	        if (Input.GetKeyDown(KeyCode.W))
-		        Win( );
-        }
-        #endif
+#if UNITY_EDITOR
+		private		void	Update		( )		
+		{
+			if (Input.GetKeyDown(KeyCode.W))
+				Win( );
+		}
+#endif
 
-        internal	void	ClickCell		( Int32 cellIndex, Boolean animate = true )															
-        {
-            var x = cellIndex % _gridSize;
-            var y = cellIndex / _gridSize;
+		internal	void	ClickCell		( Int32 cellIndex, Boolean animate = true )															
+		{
+			var x = cellIndex % _gridSize;
+			var y = cellIndex / _gridSize;
         
-            Check( x-1, y, animate );
-            Check( x+1, y, animate );
-            Check( x, y-1, animate );
-            Check( x, y+1, animate );
+			Check( x-1, y, animate );
+			Check( x+1, y, animate );
+			Check( x, y-1, animate );
+			Check( x, y+1, animate );
             
-            if (CheckWin( ))
+			if (CheckWin( ))
 			{
 				_inputBlocker.SetActive( true );
 				Win( );
 			}
             
-            return;
+			return;
 
-            void Check( Int32 x, Int32 y, Boolean animate )
-            {
+			void Check( Int32 x, Int32 y, Boolean animate )
+			{
 				if (x<0|y<0|x>=_gridSize|y>=_gridSize) 
 					return;
 
@@ -80,14 +80,14 @@ namespace Flexy.Template.BarleyBreak.CoreGame
 				{
 					MoveFigureTo( _cells[cellIndex], _cells[index], animate );
 				}
-            }
-        }
-        private		void	MoveFigureTo	( Cell sourceCell, Cell targetCell, Boolean animate = true )	
-        {
+			}
+		}
+		private		void	MoveFigureTo	( Cell sourceCell, Cell targetCell, Boolean animate = true )	
+		{
 			var figure		= sourceCell.Figure;
         
-            if (animate)
-            {
+			if (animate)
+			{
 				AnimateFigureTo( sourceCell, targetCell, _animateTime ).Forget( );
 			}
 			else
@@ -96,24 +96,24 @@ namespace Flexy.Template.BarleyBreak.CoreGame
 				figure.position	= targetCell.GlobalPosition;
 			}
 				
-            targetCell.Figure = figure;
-            sourceCell.Figure = null;
-        }
+			targetCell.Figure = figure;
+			sourceCell.Figure = null;
+		}
 	
-        private async	UniTask		InitMinigameAsync	( )		
-        {
-	        foreach (var cell in _cells)
-		        cell.ResetCell( );
+		private async	UniTask		InitMinigameAsync	( )		
+		{
+			foreach (var cell in _cells)
+				cell.ResetCell( );
         
 			//Time.timeScale = 10;
         
 			var currEmptyCell = _cells[^1];
-            for (var i = 0; i < 500; i++)
-            {
-	            var dir = Random.Range(0, 4);
+			for (var i = 0; i < 500; i++)
+			{
+				var dir = Random.Range(0, 4);
             
-	            var x = currEmptyCell.Index % _gridSize;
-	            var y = currEmptyCell.Index / _gridSize;
+				var x = currEmptyCell.Index % _gridSize;
+				var y = currEmptyCell.Index / _gridSize;
         
 				switch(dir)
 				{
@@ -136,45 +136,45 @@ namespace Flexy.Template.BarleyBreak.CoreGame
 			}
 			
 			StartTime = Time.realtimeSinceStartup;
-        }
-        private async	UniTask		AnimateFigureTo		( Cell from, Cell to, Single animationDuration = 1 ) 
-        {
+		}
+		private async	UniTask		AnimateFigureTo		( Cell from, Cell to, Single animationDuration = 1 ) 
+		{
 			var figure = from.Figure;
-	        figure.SetParent(from.transform.parent, true);
+			figure.SetParent(from.transform.parent, true);
         
-            var fromAnchoredPosition = from.GlobalPosition;
-            var toAnchoredPosition = to.GlobalPosition;
+			var fromAnchoredPosition = from.GlobalPosition;
+			var toAnchoredPosition = to.GlobalPosition;
             
-            var endTime = Time.time + animationDuration;
+			var endTime = Time.time + animationDuration;
             
-            while (Time.time < endTime)
-            {
-	            var t = 1.0f - (endTime - Time.time) / animationDuration; 
+			while (Time.time < endTime)
+			{
+				var t = 1.0f - (endTime - Time.time) / animationDuration; 
 				t = EaseUtility.SCurve(t);
             
-                figure.position	= Vector2.Lerp(fromAnchoredPosition, toAnchoredPosition, t);
+				figure.position	= Vector2.Lerp(fromAnchoredPosition, toAnchoredPosition, t);
 	        
-                await UniTask.NextFrame( );
-            }
+				await UniTask.NextFrame( );
+			}
 
-            figure.position	= to.GlobalPosition;
+			figure.position	= to.GlobalPosition;
 			figure.SetParent(to.transform, true);
-        }
+		}
         
-        private			Boolean		CheckWin			( )		
-        {
-            for (var i = 0; i < _cells.Count - 1; i++)
-            {
-                if (_cells[i].IsEmpty || !_cells[i].IsSolved)
-                    return false;
-            }
+		private			Boolean		CheckWin			( )		
+		{
+			for (var i = 0; i < _cells.Count - 1; i++)
+			{
+				if (_cells[i].IsEmpty || !_cells[i].IsSolved)
+					return false;
+			}
 
-            return true;
-        }
-        private			void		Win					( )		
-        {
-	        IsWin = true;
-	        _win.Raise( this );
-        } 
-    }
+			return true;
+		}
+		private			void		Win					( )		
+		{
+			IsWin = true;
+			_win.Raise( this );
+		} 
+	}
 }
