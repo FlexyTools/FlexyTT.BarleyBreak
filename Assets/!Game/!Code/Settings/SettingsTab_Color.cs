66870553ea067179875cfac07ebd0d5d4e308b2a
JesOb <JesObertyukh@gmail.com>
2025-08-07 08:20:47 +0200

    Added flexy.ui lite package

--- Assets/!Game/!Code/Settings/SettingsTab_Color.cs
+++ Assets/!Game/!Code/Settings/SettingsTab_Color.cs
@@ -2,5 +2,5 @@ namespace Flexy.Template.BarleyBreak.Settings;
 
 public class SettingsTab_Color : GameSettingsTab
 {
-	public ColorSetting		Primary		= new( "ColorSettingsTab_Primary",	ColorUtility.TryParseHtmlString("#EC1762", out var color) ? color : Color.gray );
+	public ColorSetting		Primary		= new( "ColorSettingsTab_Primary",	ColorUtility.TryParseHtmlString("#2E7474", out var color) ? color : Color.gray );
 }
