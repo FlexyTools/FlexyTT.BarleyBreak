afaa0472962eab7c285c7d76ab4c60265d94e886
JesOb <JesObertyukh@gmail.com>
2025-08-08 13:23:13 +0200

    Changed cleanup leaderboard window using flexy.ui collection binders

--- Assets/!Game/!Code/UI/Window_Leaderboard.cs
+++ Assets/!Game/!Code/UI/Window_Leaderboard.cs
@@ -1,57 +1,25 @@
+using Flexy.UI.Bindings;
+
 namespace Flexy.Template.BarleyBreak.UI
 {
 	public class Window_Leaderboard : UIWindowEx
 	{
-		[SerializeField]	BindableDataStore	_recordPrefab;
-		[SerializeField]	Transform			_scores_3x3;
-		[SerializeField]	Transform			_scores_4x4;
-		[SerializeField]	Transform			_scores_5x5;
-		[SerializeField]	Transform			_scores_3x3_Container;
-		[SerializeField]	Transform			_scores_4x4_Container;
-		[SerializeField]	Transform			_scores_5x5_Container;
-	
-		protected override	void	OnShow	( )		
-		{
-			ClearContainer	( _scores_3x3_Container );
-			ClearContainer	( _scores_4x4_Container );
-			ClearContainer	( _scores_5x5_Container );
-
-			_scores_3x3		.gameObject.SetActive( OpenParams is null or EField.Board3x3 );
-			_scores_4x4		.gameObject.SetActive( OpenParams is null or EField.Board4x4 );
-			_scores_5x5		.gameObject.SetActive( OpenParams is null or EField.Board5x5 );
-
-			gameObject.SetActive( true );
-
-			FillContainer	( _scores_3x3_Container, _recordPrefab, Game.Leaderboards.Leaderboard3X3.Records );
-			FillContainer	( _scores_4x4_Container, _recordPrefab, Game.Leaderboards.Leaderboard4X4.Records );
-			FillContainer	( _scores_5x5_Container, _recordPrefab, Game.Leaderboards.Leaderboard5X5.Records );
-		}
-
-		private		void	ClearContainer	( Transform container )														
-		{
-			for (var i = container.childCount - 1; i >= 0; i--) 
-				Destroy( container.GetChild(i).gameObject );
-				
-			container.DetachChildren();
-		}
-		private		void	FillContainer	( Transform container, BindableDataStore prefab, List<Single> collection )	
-		{
-			if (!container.gameObject.activeInHierarchy)
-				return;
+		[Bindable]	Collection	Records3x3 	=> new (Game.Leaderboards.Leaderboard3X3.Records, Setup);
+		[Bindable]	Collection	Records4x4 	=> new (Game.Leaderboards.Leaderboard4X4.Records, Setup);
+		[Bindable]	Collection	Records5x5 	=> new (Game.Leaderboards.Leaderboard5X5.Records, Setup);
 		
-			foreach (var item in collection)
-			{
-				var widget = Instantiate(prefab, container);
-				
-				var seconds	= item;
-				var value	= Single.IsPositiveInfinity(seconds) ? "-" : seconds >= 60 
-					? TimeSpan.FromSeconds( seconds ).ToString( @"mm\:ss\.ff" ) 
-					: TimeSpan.FromSeconds( seconds ).ToString( @"ss\.ff" );
-				
-				widget.SetValue( "Score", value );
-			}
-		}
+		[Bindable]	Boolean		IsShowBoard	( EField field )	=> OpenParams is not EField f || f == field;
 
+		private void Setup( GameObject widget, Object data, Boolean isNew, Int32 index )
+		{
+			var seconds	= (Single)data;
+			var value	= Single.IsPositiveInfinity(seconds) ? "-" : seconds >= 60 
+				? TimeSpan.FromSeconds( seconds ).ToString( @"mm\:ss\.ff" ) 
+				: TimeSpan.FromSeconds( seconds ).ToString( @"ss\.ff" );
+				
+			widget.GetComponent<BindableDataStore>( ).SetValue( "Score", value );
+		}
+		
 		public record struct Opener( OpenCtx Ctx ) : IOpener
 		{
 			public	StateHandle		Open	( )					=> Ctx.Open( null );
