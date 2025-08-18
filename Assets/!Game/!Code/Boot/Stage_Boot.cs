c7c95468c62f2601f25d96733b113bd9035f2511
JesOb <JesObertyukh@gmail.com>
2025-08-18 13:54:32 +0200

    Changed Boot to have direct references to boot states

--- Assets/!Game/!Code/Boot/Stage_Boot.cs
+++ Assets/!Game/!Code/Boot/Stage_Boot.cs
@@ -4,16 +4,18 @@
 	public class Stage_Boot : GameStageEx
 	{
 		[SerializeField]	GameObject			_loaderOverlay;
-		[SerializeField]	AssetRef<State>[]	_bootStates;
+		[SerializeField]	AssetRef<State>		_boot_eula;
 		[SerializeField]	AssetRef<GameStage>	_metaStageRef;
 	
 		[Bindable] Int32	LoadingProgress		=> (Int32)(LoadingProgress01 * 100);
         [Bindable] Single	LoadingProgress01	=> _loadTask.Progress; 
         
+        private BooleanSetting	_eulaAccepted;
 		private LoadSceneTask	_loadTask;
 
 		protected override	void	OnShow				( )		
 		{
+			_eulaAccepted	= new("Boot_EulaAccepted", false);
 			BootGame().Forget();
 		}
 		private async	UniTask		BootGame			( )		
@@ -25,7 +27,16 @@
 
 			_loaderOverlay.gameObject.SetActive(false);
 
-			foreach (var state in _bootStates)
+			if (!_eulaAccepted)
+			{
+				await ShowState(_boot_eula);
+				_eulaAccepted.Set(true);
+			}
+			
+			// await ShowState(_boot_age);
+			// await ShowState(_boot_intro);
+
+			async UniTask ShowState( AssetRef<State> state )
 			{
 				var h = Graph.Open( state, this );
 				
