7ce59572790030015d11662b182be53db7806dd4
JesOb <JesObertyukh@gmail.com>
2025-10-27 20:12:38 +0100

    Added leaderboard button options

--- Assets/!Game/!Code/Metagame/Leaderboards/Window_Leaderboards.cs
+++ Assets/!Game/!Code/Metagame/Leaderboards/Window_Leaderboards.cs
@@ -8,24 +8,23 @@ namespace FlexyTemplates.BarleyBreak.Metagame.Leaderboards
 		[Bindable]	Collection	Records4x4 	=> new (Game.Leaderboards.Board4X4.Records.Select(s => new ScoreView(s)));
 		[Bindable]	Collection	Records5x5 	=> new (Game.Leaderboards.Board5X5.Records.Select(s => new ScoreView(s)));
 		
+		[Bindable]	Boolean		ExitToRight	=> OpenParams is not null;
 		[Bindable]	Boolean		IsShowBoard	( EField field )	=> OpenParams is not EField f || f == field;
-		[Bindable]	String		Bla1	( EField field )	=> "Bla String";
-		[Bindable]	String		Bla2		=> "Bla String";
+		
+		[StateTest]	Object		Board3x3	( ) => EField.Board3x3;
+		[StateTest]	Object		Board4x4	( ) => EField.Board4x4;
+		[StateTest]	Object		Board5x5	( ) => EField.Board5x5;
+		
+		private record ScoreView(Single Score)
+		{
+			[Bindable]	Single	Score		{get;init;} = Score;
+			[Bindable]	String	ScoreStr	=> Single.IsPositiveInfinity(Score) ? "-" : TimeSpan.FromSeconds( Score ).ToString( Score >= 60 ? @"mm\:ss\.ff" : @"ss\.ff" );
+		}
 		
 		public record struct Opener( OpenCtx Ctx ) : IOpener
 		{
 			public	StateHandle		Open	( )					=> Ctx.Open( null );
 			public	StateHandle		Open	( EField board )	=> Ctx.Open( board );
-		}
-		
-		[StateTest]	Object	Board3x3	( ) => EField.Board3x3;
-		[StateTest]	Object	Board4x4	( ) => EField.Board4x4;
-		[StateTest]	Object	Board5x5	( ) => EField.Board5x5;
-		
-		private record ScoreView(Single Score)
-		{
-			[Bindable]	Single Score	{get;init;} = Score;
-			[Bindable]	String ScoreStr	=> Single.IsPositiveInfinity(Score) ? "-" : TimeSpan.FromSeconds( Score ).ToString( Score >= 60 ? @"mm\:ss\.ff" : @"ss\.ff" );
 		}
 	}
 }
