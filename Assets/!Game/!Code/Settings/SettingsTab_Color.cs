b044e530df0cda49a7711ff5000e9ba89dc4babf
JesOb <JesObertyukh@gmail.com>
2025-11-02 21:22:32 +0100

    Changed deafult border color

--- Assets/!Game/!Code/Settings/SettingsTab_Color.cs
+++ Assets/!Game/!Code/Settings/SettingsTab_Color.cs
@@ -2,5 +2,5 @@ namespace FlexyTemplates.BarleyBreak.Settings;
 
 public class SettingsTab_Color : GameSettingsTab
 {
-	public ColorSetting		Primary		= new( "ColorSettingsTab_Primary",	ColorUtility.TryParseHtmlString("#2E7474", out var color) ? color : Color.gray );
+	public ColorSetting		Primary		= new( "ColorSettingsTab_Primary",	ColorUtility.TryParseHtmlString("#8F48AC", out var color) ? color : Color.gray );
 }
